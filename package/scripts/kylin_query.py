a3b065696acb61446cf6163f65ba3726b7374f03
tkiyer <jiyidexin@gmail.com>
2019-04-30 14:13:22 +0800

    加上Kylin的Conf文件夹

--- package/scripts/kylin_query.py
+++ package/scripts/kylin_query.py
@@ -21,6 +21,8 @@ class KylinQuery(Script):
         Execute('rm -rf /tmp/kylin.tar.gz')
         # Rename kylin installation dir
         Execute('mv /usr/local/{0} {1}'.format(params.kylin_project_name, params.kylin_install_dir))
+        # Create kylin conf dir
+        Exeucte('mkdir -p {0}/conf'.format(params.kylin_install_dir))
         # Create Hadoop conf dir
         Execute('mkdir -p {0}/hadoop-conf'.format(params.kylin_install_dir))
         # Create kylin user and group
