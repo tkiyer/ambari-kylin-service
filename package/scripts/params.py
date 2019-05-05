f1d8838edc9a9e6ac06baa65a53d93039a1f500a
tkiyer <jiyidexin@gmail.com>
2019-05-05 12:07:40 +0800

    修复下载错误

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -37,6 +37,8 @@ kylin_tomcat_context_conf = config['configurations']['kylin-tomcat-context']['ky
 
 kylin_spark_download_sh = config['configurations']['kylin-spark-download']['kylin_spark_download_sh']
 
+spark_download_base_url = config['configurations']['kylin-spark-download']['spark_download_base_url']
+
 # kylin web timezone
 kylin_web_timezone = config['configurations']['kylin']['kylin_web_timezone']
 # kylin_web_cross_domain_enabled
